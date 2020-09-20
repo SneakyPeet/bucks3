0152788d46a583baa93affcec891edb7b8ec7958
Pieter Koornhof <koornhof.pieter@gmail.com>
2020-09-20 13:42:18 +0400

    Chore: save restore

--- src/bucks/app.cljs
+++ src/bucks/app.cljs
@@ -6,7 +6,10 @@
             [bucks.accounts.state :as accounts]
             [bucks.pages.registry :as pages]
             [bucks.options.state :as options]
-            [akiroz.re-frame.storage :refer [reg-co-fx!]]))
+            [akiroz.re-frame.storage :refer [reg-co-fx!]]
+            ["file-saver" :as file-saver]
+            ["moment" :as moment]
+            [cljs.reader :refer [read-string]]))
 
 
 (reg-co-fx! :bucks3
@@ -33,8 +36,61 @@
     :dispatch [::initialize]}))
 
 
+(rf/reg-event-fx
+ ::save
+ [(rf/inject-cofx :localstore)]
+ (fn [{:keys [localstore]} _]
+   (let [now (.format (moment) "YYYY-MM-DD-hh-mm")
+         name (str now "-bucks.txt")
+         content (pr-str (update localstore :options dissoc :fixer-api-key))
+         blob (js/Blob. (array content) #js {:type "text/plain;charset=utf-8"})]
+     (.saveAs file-saver blob name))
+   {:localstore localstore}))
+
+
+(rf/reg-event-fx
+ ::restore
+ [(rf/inject-cofx :localstore)]
+ (fn [{:keys [localstore]} [_ data]]
+   {:localstore (update data :options (fn [o] (merge (:options localstore) o)))
+    :dispatch [::initialize]}))
+
+
+(defn- read-files [files]
+  (try
+    (let [file (aget files 0)
+          reader (js/FileReader.)]
+      (set! (.-onload reader)
+            (fn [e]
+              (prn 1)
+              (rf/dispatch [::restore (read-string (.. e -target -result))])))
+      (.readAsText reader file))
+    (catch :default e
+      (js/alert e))))
+
+
+(defn nav []
+  [:nav.navbar
+   [:div.navbar-menu
+    [:div.navbar-end
+     [:div.navbar-item
+      [:div.buttons.mb-0 {:style {:align-items "flex-start"}}
+       [:button.button.is-small
+        {:on-click #(rf/dispatch [::save])}
+        "save"]
+       [:div.file.is-small
+        [:label.file-label
+         [:input.file-input {:type "file" :name "restore" :accept ".txt"
+                             :on-change (fn [e]
+                                          (read-files (.. e -target -files))
+                                          (set! (.. e -target -value) ""))}]
+         [:span.file-cta
+          [:span.file-label "restore"]]]]]]
+     ]]])
+
 (defn app []
-  [:div.section
+  [:div.section.pt-0
+   [nav]
    [:div.container
     [pages/component]]])
 
